808ce15aad82d6175fb2f87b9ab96b817f43f063
David Jeong <drumrobot43@gmail.com>
2022-10-28 17:20:04 +0900

    2022-10-28

--- ch01/blocking.js
+++ ch01/blocking.js
@@ -1,7 +1,7 @@
 /* blocking.js */
 function longRunningTask() {
   // 오래 걸리는 작업
-  for (let i = 0; i < 9999999999; i++);
+  for (let i = 0; i < 2222222222; i++);
   console.log('작업 끝');
 }
 console.log('시작');
---
+++ ch06/6.1/learn-express/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  // res.send('Hello, Express');
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  let port = app.get('port');
+  console.log(`http://localhost:${port} 에서 대기 중`);
+});
